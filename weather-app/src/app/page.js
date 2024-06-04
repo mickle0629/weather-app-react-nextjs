e73462b9c5a23a3796b77f1b05252bce9a234661
mickle0629 <mikeyd2k2@gmail.com>
2024-06-04 00:02:30 -0700

    todo update

--- weather-app/src/app/page.js
+++ weather-app/src/app/page.js
@@ -7,7 +7,7 @@ export default async function Page() {
   //fetching data...
   const currentWeather = await fetchCurrentData();
   console.log(currentWeather.forecast.forecastday);
-
+  //TODO: parse time into better format
   const currWeatherCardData = {
     top: currentWeather.location.localtime,
     main: `${currentWeather.current.temp_c}\u00b0C`,
@@ -19,18 +19,16 @@ export default async function Page() {
     main: `${Math.trunc(currentWeather.forecast.forecastday[0].day.mintemp_c)} - ${Math.trunc(currentWeather.forecast.forecastday[0].day.maxtemp_c)}\u00b0C`,
     bottom: ''
   }
-  
+
   const precipCardData = {
     top: 'Precipitation',
     main: `${currentWeather.forecast.forecastday[0].day.totalprecip_mm}mm`,
-    //TODO: Dynamic rain descriptors
     bottom: `${getRainDescription(currentWeather.forecast.forecastday[0].day.totalprecip_mm)}`
   }
 
   const uviCardData = {
     top: 'UV Index',
     main: `${currentWeather.forecast.forecastday[0].day.uv}`,
-    //TODO: Display different UV warnings based on UVI
     bottom: `${getUVWarnings(currentWeather.forecast.forecastday[0].day.uv)}`
   }
 
